fe598040ec8ef6d5c0d08108b6e0c0b516c0e977
t0w2 <tony.ww@gmail.com>
2017-11-01 09:40:02 -0400

    Update datastructures.cpp

--- src/datastructures.cpp
+++ src/datastructures.cpp
@@ -1,6 +1,6 @@
 #include "../libs/structures.h"
 
-// Comments added from t0w2-patch-1 for the 14th time.
+// Comments added from t0w2-patch-1 for the 15th time.
 int main(int argc, char* argv[])
 {
     own_array<int> myarray;
